5480b740def1bfe933b996459cdbda6bd9f1abbe
kamlesh5113 <kamleshleel5113@gmail.com>
2022-08-02 21:44:35 +0530

    make it better

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ app.use((error,req,res,next) =>{                        // to catch error from u
   });
 
 
-app.listen(process.env.PORT || 5000,(req,res)=>{
+app.listen(process.env.PORT || 5000, ()=>{
     console.log("Connected to server");
 })
